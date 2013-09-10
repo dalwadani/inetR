656836bb9df967c4cceada075749fca02590c257
Dhaifallah Alwadani <dalwadani@gmail.com>
2013-09-10 19:47:34 +0100

    initiial commit

---
+++ test.R
@@ -0,0 +1,53 @@
+require(omnetpp)
+
+x <- loadDataset("results/*.sca")
+sc <- x$scalars[x$scalars$value != 0,]
+sc$runid <- NULL
+sc$resultkey <- NULL
+head(sc)
+routers <- sc[ grepl("routers",ignore.case = TRUE,sc$module),]
+clients <- sc[ grepl("Clients",ignore.case = TRUE,sc$module),]
+
+wc <- reshape(clients,
+              timevar = "name",
+              idvar = c("file","module"),
+              direction = "wide")
+
+c0 <- clients[ grepl("Clients\\[0\\]",ignore.case = TRUE,clients$module),]
+
+
+routers  <- droplevels(routers)
+levels(routers$module)
+clients  <- droplevels(clients)
+levels(clients$module)
+
+c0 <- droplevels(c0)
+levels(c0$module)
+levels(c0$name)
+
+nrow(c0[grepl("udpApp",c0$module),])
+nrow(c0)
+c0[100,]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
